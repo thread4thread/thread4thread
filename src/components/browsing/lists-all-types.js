c1e2602406778afd3084d6eae190fc9244d6e3b8
Jasper Maynard-Zhang <maynjas19@gmail.com>
2023-03-24 16:08:47 -0700

    Add ListingTypeSection to Listings of All Types

--- src/components/browsing/lists-all-types.js
+++ src/components/browsing/lists-all-types.js
@@ -6,83 +6,57 @@
 // PACKAGES
 import { NavLink } from 'react-router-dom';
 
-// DATA
-import pinkSkirtImg from './../../assets/img/pink-skirt.jpg';
-
 // HELPERS
 import NavList from './browsing.helpers/nav-list';
+
+// DATA
+import pinkSkirtImg from './../../assets/img/pink-skirt.jpg';
+// const listingObj = {
+
+// }
+let altProp = "Black tank top, pink+purple skirt";
+let classNProp = "grid-item";
 
 // TODO: Display filters applied
 
 export function ListsAllTypes() {
-    let altProp = "Black tank top, pink+purple skirt";
-    let classNProp = "grid-item";
-
     return (
         <div className="side-wrap box column">
+            {/* <!-- Offers (Donations) --> */}
+            <ListingTypeSection sectionTitle="Offers"/>
 
-        {/* <!-- Place holder--> */}
-        {/* <!-- <div className="place-holder box column top-bot-wrap"></div> --> */}
-        {/* <!-- Offers (Donations) --> */}
+            {/* <!-- Trade Offers --> */}
+            <ListingTypeSection sectionTitle="Trade Offers"/>
+
+            {/* <!-- Requests --> */}
+            <ListingTypeSection sectionTitle="Reguests"/>
+        </div>
+    )
+}
+
+function ListingTypeSection(props) {
+    let { sectionTitle } = props;
+    // let { listingsObj } = props;
+    // let { pinkShirtImg, altProp, classNProp } = listingsObj;
+
+    return (
         <div className="box column top-bot-wrap">
-            {/* <!-- Offers heading + "See more" button --> */}
-            <div className="box">
-                <h1 className="left-item">Offers</h1>
-                <SeeMoreButton/>
-            </div>
-
-            {/* <!-- Offer listings grid --> */}
-            <div className="grid by-3">
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-            </div>
+        {/* <!-- Offers heading + "See more" button --> */}
+        <div className="box">
+            <h1 className="left-item">{sectionTitle}</h1>
+            <SeeMoreButton/>
         </div>
 
-        {/* <!-- Trade Offers --> */}
-        <div className="box column top-bot-wrap">
-            {/* <!-- top-bot-wrap heading + "See more" button --> */}
-            <div className="box">
-                <h1 className="left-item">Trade Offers</h1>
-                <SeeMoreButton/>
-            </div>
-
-            {/* <!-- Request listings grid --> */}
-            <div className="grid by-3">
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-            </div>
+        {/* <!-- Offer listings grid --> */}
+        <div className="grid by-3">
+            <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
+            <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
+            <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
+            <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
+            <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
+            <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
         </div>
-
-        {/* <!-- Requests --> */}
-        <div className="box column top-bot-wrap">
-            {/* <!-- top-bot-wrap heading + "See more" button --> */}
-            <div className="box">
-                <h1 className="left-item">Requests</h1>
-                <SeeMoreButton/>
-            </div>
-
-            {/* <!-- Request listings grid --> */}
-            <div className="grid by-3">
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-                <NavList src={pinkSkirtImg} navTo="list-details" alt={altProp} classN={classNProp}/>
-            </div>
-        </div>
-        {/* <!-- Place holder--> */}
-        {/* <!-- <div className="place-holder-bottom"></div> --> */}
-
-        </div>
+    </div>
     )
 }
 
