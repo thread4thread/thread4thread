35446c262249efb60c104defe4a66629324447c3
Grace Gibbons <ggibbons@uw.edu>
2023-04-28 15:42:58 -0700

    fixed welcome page

--- src/components/account/welcome.js
+++ src/components/account/welcome.js
@@ -5,21 +5,24 @@
 // import ArrowIcon from './../../assets/icon/keyboard-arrow-return.png'
 
 import { Link } from 'react-router-dom';
+import logoPic  from '../../components/navbar/navbar.assets/logo2.png';
 
 export function Welcome() {
     return (
-        <main class="content-wrap">
-        <div class="login">
-            <img src="../img/t4t-Logo" alt="link to index" class="logos-welcome"/>
-            <h1 class="welcome-h">Welcome to thread4thread</h1>
-        </div>
-        <div class="welcome">
-            <Link to="/login">
-            <button type="button" class="purp-button-welcome">Login</button>
-            </Link>
-            <Link to="/sign-up">
-            <button type="button" class="purp-button-welcome">Sign up</button>
-            </Link>
+     <main className="content-wrap">
+        <div className='justify-content-center'>
+            <div className="login">
+                <img src={logoPic} alt="link to index" className="logos-welcome"/>
+                <h1 className="welcome-h">Welcome to thread4thread</h1>
+            </div>
+            <div className="welcome">
+                <Link to="/login">
+                <button type="button" className="purp-button-welcome">Login</button>
+                </Link>
+                <Link to="/sign-up">
+                <button type="button" className="purp-button-welcome">Sign up</button>
+                </Link>
+            </div>
         </div>
     </main>
     )
