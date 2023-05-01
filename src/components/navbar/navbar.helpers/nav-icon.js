be59e3493e4aeaa9c137c6347904723caf984f99
Grace Gibbons <ggibbons@uw.edu>
2023-04-30 20:05:29 -0700

    made nav-bar responsive, added escape to form

--- src/components/navbar/navbar.helpers/nav-icon.js
+++ src/components/navbar/navbar.helpers/nav-icon.js
@@ -6,6 +6,7 @@
 
 // PACKAGES
 import { NavLink } from 'react-router-dom';
+import React from 'react';
 
 // TODO: Do all button/icon creation in one file, then import where needed?
 
@@ -13,11 +14,29 @@ import { NavLink } from 'react-router-dom';
 export default function NavIcon(props) {
     let {src, classN, navPath} = props;
     let iName = src + "";
+
+    function handleClick(event) {
+        // event.preventDefault();
+        event.target.classList.toggle('active');
+        //console.log(iName);
+        if(event.target.classList.contains('active')) {
+            let newiName = iName.indexOf('.');
+            src = iName.slice(0, newiName) + "-active" + iName.slice(newiName);
+            
+        } 
+        //console.log(src);
+        // else {
+        // let index = applyFilters.indexOf(event.target.value)
+        // applyFilters.splice(index, 1);
+        // }
+    }
     
     // TODO: For some reason, only "navbar-brand" works with top-nav icons and "col" works with bot-nav icons. It would be great if there could just be 1 class that works for both.
     return (
-        <NavLink to={navPath} className={classN}>
+        <NavLink onClick={handleClick} to={navPath} className={classN}>
             <img src={src} alt={iName}/>
         </NavLink>
     )
 }
+
+
