e01acc0065cc69336f95dca474baa60fcdd13481
Grace Gibbons <ggibbons@uw.edu>
2023-05-03 10:41:14 -0700

    worked on nav bar responsiveness

--- src/components/navbar/navbar.helpers/nav-icon.js
+++ src/components/navbar/navbar.helpers/nav-icon.js
@@ -7,6 +7,10 @@
 // PACKAGES
 import { NavLink } from 'react-router-dom';
 import React from 'react';
+import { accountActive} from '../../../assets/icon/account-active.png'
+import { homeActive} from '../../../assets/icon/home-active.png'
+import { mailActive} from '../../../assets/icon/mail-active.png'
+import { addActive} from '../../../assets/icon/add-active.png'
 
 // TODO: Do all button/icon creation in one file, then import where needed?
 
@@ -17,19 +21,33 @@ export default function NavIcon(props) {
 
     function handleClick(event) {
         // event.preventDefault();
-        event.target.classList.toggle('active');
+        let newiName = iName.indexOf('.');
+        let iconName = iName.slice(28, newiName);
+        event.target.classList.toggle(iconName);
+        //event.target.classList.toggle('active');
+       // console.log(event.target.classList);
         //console.log(iName);
-        if(event.target.classList.contains('active')) {
-            let newiName = iName.indexOf('.');
-            src = iName.slice(0, newiName) + "-active" + iName.slice(newiName);
-            
-        } 
+        // if(event.target.classList.contains('active')) {
+        //     src = iName.slice(0, newiName) + "-active" + iName.slice(newiName);
+        //     console.log(src);
+        // } 
+
+        if(event.target.classList.contains(iconName)) {
+            src = '../../../assets/icon/' + iconName + '-active.png';
+            console.log(src);
+        }
+
+        //console.log(src);
+
         //console.log(src);
         // else {
         // let index = applyFilters.indexOf(event.target.value)
         // applyFilters.splice(index, 1);
         // }
     }
+
+   // console.log(src);
+
     
     // TODO: For some reason, only "navbar-brand" works with top-nav icons and "col" works with bot-nav icons. It would be great if there could just be 1 class that works for both.
     return (
