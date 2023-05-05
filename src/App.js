26c7bf4d37384fad8dbddeb8a43c8eda0e91aaef
Jasper Maynard-Zhang <maynjas19@gmail.com>
2023-05-05 13:37:16 -0700

    Remove duplicate my-profile.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ import { SearchFilter } from './components/browsing/search-filter';
 import { ListingDetails } from './components/browsing/list-details';
 import { CreateList } from './components/posting/create-list';
 import { LogIn } from './components/account/login';
-import { Profile } from './components/account/my-profile';
 // import {LogOutModal} from './components/account/logout';
 import { UserProfile } from './components/profile/user-profile';
 
@@ -62,8 +61,6 @@ function App() {
         <Route path='/my-profile' element={<UserProfile/>} />
 
         <Route path='/login' element={<LogIn/>}/>
-
-        <Route path='/my-profile' element={<Profile/>}/>
 
         {/* <Route path='/logout' element={<LogOutModal/>}/> */}
 
