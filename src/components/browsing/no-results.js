eb7889fd271f5bf8b53669df1960898755cd02b6
Grace Gibbons <ggibbons@uw.edu>
2023-05-12 15:06:46 -0700

    fixed logout btn and list-all-types

--- src/components/browsing/no-results.js
+++ src/components/browsing/no-results.js
@@ -1,7 +1,7 @@
 export default function NoResults () {
     return(
         <div className="csoon container">
-            <h1 className="messageR">Sorry! No results match your search.</h1>
+            <h1 className="messageR">No listings yet.</h1>
         </div>
     )
 
